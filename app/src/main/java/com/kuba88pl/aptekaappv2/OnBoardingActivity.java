d1ea39fee204c89ec49135ace42e6d8dd3dbbc4d
Kuba88pl <jakub.kulawik@gmail.com>
2023-06-02 12:17:54 +0200

    SliderAdapter working at

---
+++ app/src/main/java/com/kuba88pl/aptekaappv2/OnBoardingActivity.java
@@ -0,0 +1,14 @@
+package com.kuba88pl.aptekaappv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
