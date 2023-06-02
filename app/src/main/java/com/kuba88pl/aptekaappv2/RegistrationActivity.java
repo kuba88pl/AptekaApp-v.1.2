05fdc609492a00b686ec24bfc04ba374820e9d1e
Kuba88pl <jakub.kulawik@gmail.com>
2023-06-02 11:19:23 +0200

    RegistrationActivity in progress

--- app/src/main/java/com/kuba88pl/aptekaappv2/RegistrationActivity.java
+++ app/src/main/java/com/kuba88pl/aptekaappv2/RegistrationActivity.java
@@ -4,18 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    EditText name, email, password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
     }
 
     public void signup(View view) {
+
+        String userName = name.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if(TextUtils.isEmpty(userName)){
+            Toast.makeText(this, "Podaj nazwę użytkownika!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this, "Podaj adres e-mail!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(userPassword)){
+            Toast.makeText(this, "Podaj hasło!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(userPassword.length() < 6 ){
+            Toast.makeText(this, "Hasło powinno składać się z conajmniej 6 znaków!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+
     }
 
     public void signin(View view) {
