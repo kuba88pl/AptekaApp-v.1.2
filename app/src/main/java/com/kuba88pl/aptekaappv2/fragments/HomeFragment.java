0bd176f1363d5f8b70c00582b50bb8eaadb5ff37
Kuba88pl <jakub.kulawik@gmail.com>
2023-06-02 14:43:10 +0200

    CategoryModel, CategoryAdapter created, Firestore connection works fine

--- app/src/main/java/com/kuba88pl/aptekaappv2/fragments/HomeFragment.java
+++ app/src/main/java/com/kuba88pl/aptekaappv2/fragments/HomeFragment.java
@@ -2,7 +2,10 @@ package com.kuba88pl.aptekaappv2.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +14,14 @@ import android.view.ViewGroup;
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.kuba88pl.aptekaappv2.R;
+import com.kuba88pl.aptekaappv2.adapters.CategoryAdapter;
+import com.kuba88pl.aptekaappv2.models.CategoryModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +29,16 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    RecyclerView catRecyclerview;
+
+
+    //Category recyclerview
+    CategoryAdapter categoryAdapter;
+    List<CategoryModel> categoryModelList;
+
+    //FireStore
+
+    FirebaseFirestore db;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -29,15 +49,43 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        catRecyclerview =  root.findViewById(R.id.rec_category);
+
+        db = FirebaseFirestore.getInstance();
+
         //image slider
         ImageSlider imageSlider = root.findViewById(R.id.image_slider);
         List<SlideModel> slideModels = new ArrayList<>();
 
         slideModels.add(new SlideModel(R.drawable.banner1,"Ibuprom w niższej cenie!", ScaleTypes.CENTER_CROP));
-        slideModels.add(new SlideModel(R.drawable.banner2,"Na męskie problemy!", ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel(R.drawable.banner2,"Coś na męskie problemy!", ScaleTypes.CENTER_CROP));
         slideModels.add(new SlideModel(R.drawable.banner3,"Coś dobrego dla Twojego dziecka!", ScaleTypes.CENTER_CROP));
 
         imageSlider.setImageList(slideModels);
+
+        //Category
+        catRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        categoryModelList = new ArrayList<>();
+        categoryAdapter = new CategoryAdapter(getContext(), categoryModelList);
+        catRecyclerview.setAdapter(categoryAdapter);
+
+        db.collection("Category")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                CategoryModel categoryModel = document.toObject(CategoryModel.class);
+                                categoryModelList.add(categoryModel);
+                                categoryAdapter.notifyDataSetChanged();
+
+                            }
+                        } else {
+
+                        }
+                    }
+                });
 
         return root;
     }
